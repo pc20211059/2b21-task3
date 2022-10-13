3112a8ad40abfa2c9054f9a499b4d46a43d7f5a8
pc20211059 <115680084+pc20211059@users.noreply.github.com>
2022-10-13 11:04:44 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (randint(1, 6)))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(109876543210)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
